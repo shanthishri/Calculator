dbcee1a7ce06af82fb38e17d7c505a209c848935
shanthishri <shanthishri@users.noreply.github.com>
2020-11-28 23:40:17 +0530

    Update StringCalculator.java

--- StringCalculator.java
+++ StringCalculator.java
@@ -2,8 +2,37 @@ import java.io.*;
 
 class StringCalculator 
 {
-	public static void main(String args[])
+	public static void main(String args[]) throws IOException
 	{
+		String num;
+		int sum=0;
+		DataInputStream d1 = new DataInputStream(System.in);
 		
+		try {
+	
+            System.out.println("Enter number: ");
+		    num = d1.readLine();
+		    sum = Add(num);
+            System.out.println("Sum of numbers is: "+sum);
+        }
+        catch(NumberFormatException e)
+       {
+    	   System.out.println("sum is 0");
+       } 
+	}
+
+	static int Add(String x) 
+	{
+		int sum =0;
+        int len = x.length();
+        
+        String[] arrOfStr = x.split(",", len); 
+  
+        for (String a : arrOfStr) 
+        {
+			sum = sum + Integer.parseInt(a);
+        }
+          
+		return sum;
 	}
 }
